2376e986694fd516da0713c674c319f365c67082
Jacob <Jacob85petersen@gmail.com>
2023-03-27 12:45:30 -0500

    Did Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 12);
+        Rectangle rectangle = new Rectangle("red", 12, 24);
+        Circle circle = new Circle("Purple", 10);
+        List<Shape> shapes = new List<Shape>
+        {
+            {square}, {rectangle}, {circle}
+        };
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
